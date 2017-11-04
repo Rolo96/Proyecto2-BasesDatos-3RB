64effdd3d40c53eac14e775df1ed15e3094b074e
weberbryan40 <weber.bryan40@gmail.com>
2017-11-04 16:52:08 -0600

    Scripts de creación y populacion de la base de datos POS-GasStationPharmacy

---
+++ BD/Creacion-POS-GasStationPharmacy.sql
@@ -0,0 +1,199 @@
+CREATE TABLE COMPANIA(
+	Nombre varchar(50) NOT NULL,
+	Sede varchar(50),
+	Activo bit NOT NULL,
+	PRIMARY KEY (Nombre)
+);
+
+CREATE TABLE SUCURSAL(
+	Nombre varchar(50) NOT NULL,
+	Provincia varchar(20) NOT NULL,
+	Ciudad varchar(20),
+	Senas varchar(50),
+	Descripcion varchar(50),
+    Activo bit NOT NULL,
+	Compania varchar(50) NOT NULL,
+	PRIMARY KEY (Nombre)
+);
+
+CREATE TABLE CAJA(
+	Id int NOT NULL,
+    Activo bit NOT NULL,
+    Sucursal varchar(50) NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE EMPLEADO(
+	Cedula int NOT NULL,
+	Nombre1 varchar(20) NOT NULL,
+	Nombre2 varchar(20),
+	Apellido1 varchar(20) NOT NULL,
+	Apellido2 Varchar(20),
+	Provincia varchar(20) NOT NULL,
+	Cuidad varchar(20),
+	Senas varchar(50),
+	FechaNacimiento date NOT NULL,
+	Contrasena varchar(200) NOT NULL,
+	Activo bit NOT NULL,
+    Sucursal varchar(50) NOT NULL,
+	Rol varchar(20) NOT NULL,
+	PRIMARY KEY (Cedula)
+);
+
+CREATE TABLE ROL(
+	Nombre varchar(20) NOT NULL,
+	Descripcion varchar(50),
+	Activo bit NOT NULL,
+	PRIMARY KEY (Nombre)
+);
+
+CREATE TABLE CLIENTE(
+	Cedula int NOT NULL,
+	Nombre1 varchar(20) NOT NULL,
+	Nombre2 varchar(20),
+	Apellido1 varchar(20) NOT NULL,
+	Apellido2 varchar(20),
+	Provincia varchar(20) NOT NULL,
+	Cuidad varchar(30),
+	Senas varchar(50),
+	FechaNacimiento date NOT NULL,
+    Contrasena varchar(200) NOT NULL,
+	Prioridad int NOT NULL,
+	Activo bit NOT NULL,
+	PRIMARY KEY (Cedula)
+);
+
+CREATE TABLE TELEFONOXCLIENTE(
+	Cliente int NOT NULL,
+	Telefono int NOT NULL,
+	Activo bit NOT NULL,
+	PRIMARY KEY (Cliente,Telefono)
+);
+
+CREATE TABLE PADECIMIENTO(
+    Cliente int NOT NULL,
+	Padecimiento varchar(50) NOT NULL,
+	Ano int NOT NULL,
+	Activo bit NOT NULL,
+	PRIMARY KEY (Cliente,Padecimiento,Ano)
+);
+
+CREATE TABLE PROVEEDOR(
+	Nombre varchar(50) NOT NULL,
+	Sede varchar(50),
+	Activo bit NOT NULL,
+	PRIMARY KEY (Nombre)
+);
+
+CREATE TABLE MEDICAMENTO(
+	Nombre varchar(50) NOT NULL,
+    Precio int NOT NULL,
+	Prescripcion bit NOT NULL,
+    Activo bit NOT NULL,
+	Proveedor varchar(50) NOT NULL,
+	PRIMARY KEY (Nombre)
+);
+
+CREATE TABLE FACTURA(
+	Id int NOT NULL,
+    Fecha date NOT NULL,
+    Hora time NOT NULL,
+    Total int NOT NULL,
+    Activo bit NOT NULL,
+    Caja int NOT NULL,
+    Empleado int NOT NULL,
+    Cliente int NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE ADMINISTRADORXSUCURSAL(
+	Administrador int NOT NULL,
+	Sucursal varchar(50) NOT NULL,
+	Activo bit NOT NULL,
+	PRIMARY KEY (Administrador,Sucursal)
+);
+
+CREATE TABLE MEDICAMENTOXFACTURA(
+	Medicamento varchar(50) NOT NULL,
+    Factura int NOT NULL,
+    Cantidad int NOT NULL,
+    Activo bit NOT NULL,
+    PRIMARY KEY(Medicamento,Factura)
+);
+
+CREATE TABLE MEDICAMENTOXSUCURSAL(
+	Medicamento varchar(50) NOT NULL,
+    Sucursal varchar(50) NOT NULL,
+    Cantidad int NOT NULL,
+    StockMinimo int NOT NULL,
+    StockPromedio int NOT NULL,
+    Activo bit NOT NULL,
+    PRIMARY KEY(Medicamento,Sucursal)
+);
+
+CREATE TABLE EMPLEADOXCAJA(
+	Empleado int NOT NULL,
+    Caja int NOT NULL,
+    FechaInicio date NOT NULL,
+    FechaFinal date NOT NULL,
+    HoraInicio time NOT NULL,
+    HoraFinal time NOT NULL,
+    EfectivoInicial int NOT NULL,
+    EfectivoFinal int NOT NULL,
+    Activo bit NOT NULL,
+    PRIMARY KEY(Empleado,Caja)
+);
+
+ALTER TABLE SUCURSAL 
+ADD CONSTRAINT FK_COMP_SUCUR FOREIGN KEY (Compania) REFERENCES COMPANIA(Nombre);
+
+ALTER TABLE EMPLEADO 
+ADD CONSTRAINT FK_SUCUR_EMP FOREIGN KEY (Sucursal) REFERENCES Sucursal(Nombre);
+
+ALTER TABLE EMPLEADO 
+ADD CONSTRAINT FK_ROL_EMP FOREIGN KEY (Rol) REFERENCES ROL(Nombre);
+
+ALTER TABLE CAJA 
+ADD CONSTRAINT FK_SUCUR_CAJA FOREIGN KEY (Sucursal) REFERENCES SUCURSAL(Nombre);
+
+ALTER TABLE MEDICAMENTO 
+ADD CONSTRAINT FK_PROVEEDOR_MEDI FOREIGN KEY (Proveedor) REFERENCES PROVEEDOR(Nombre);
+
+ALTER TABLE FACTURA 
+ADD CONSTRAINT FK_CAJA_FAC FOREIGN KEY (Caja) REFERENCES CAJA(Id);
+
+ALTER TABLE FACTURA 
+ADD CONSTRAINT FK_EMP_FAC FOREIGN KEY (Empleado) REFERENCES EMPLEADO(Cedula);
+
+ALTER TABLE FACTURA 
+ADD CONSTRAINT FK_CLI_FAC FOREIGN KEY (Cliente) REFERENCES CLIENTE(Cedula);
+
+ALTER TABLE PADECIMIENTO 
+ADD CONSTRAINT FK_CLIENTE_PED FOREIGN KEY (Cliente) REFERENCES CLIENTE(Cedula);
+
+ALTER TABLE ADMINISTRADORXSUCURSAL
+ADD CONSTRAINT FK_ADMIN_SUCUR FOREIGN KEY (Administrador) REFERENCES EMPLEADO(Cedula);
+
+ALTER TABLE ADMINISTRADORXSUCURSAL
+ADD CONSTRAINT FK_SUCUR_ADMIN FOREIGN KEY (Sucursal) REFERENCES Sucursal(Nombre);
+
+ALTER TABLE TELEFONOXCLIENTE 
+ADD CONSTRAINT FK_CLIENTE_TEL FOREIGN KEY (Cliente) REFERENCES CLIENTE(Cedula);
+
+ALTER TABLE MEDICAMENTOXSUCURSAL 
+ADD CONSTRAINT FK_MEDICA_SUCUR FOREIGN KEY (Medicamento) REFERENCES MEDICAMENTO(Nombre);
+
+ALTER TABLE MEDICAMENTOXSUCURSAL 
+ADD CONSTRAINT FK_SUCUR_MEDICA FOREIGN KEY (Sucursal) REFERENCES SUCURSAL(Nombre);
+
+ALTER TABLE MEDICAMENTOXFACTURA
+ADD CONSTRAINT FK_MEDICA_FACT FOREIGN KEY (Medicamento) REFERENCES MEDICAMENTO(Nombre);
+
+ALTER TABLE MEDICAMENTOXFACTURA 
+ADD CONSTRAINT FK_FACT_MEDICA FOREIGN KEY (Factura) REFERENCES FACTURA(Id);
+
+ALTER TABLE EMPLEADOXCAJA
+ADD CONSTRAINT FK_EMP_CAJA FOREIGN KEY (Empleado) REFERENCES EMPLEADO(Cedula);
+
+ALTER TABLE EMPLEADOXCAJA
+ADD CONSTRAINT FK_CAJA_EMP FOREIGN KEY (Caja) REFERENCES CAJA(Id);
---
+++ BD/Populacion-POS-GasStationPharmacy.sql
@@ -0,0 +1,267 @@
+INSERT INTO COMPANIA (Nombre,Sede,Activo)
+     VALUES('Farmacias Phischel', 'Costa Rica', '1');
+
+INSERT INTO COMPANIA (Nombre,Sede,Activo)
+     VALUES('BombaTica', 'Costa Rica', '1');
+
+INSERT INTO SUCURSAL (Nombre, Provincia, Ciudad, Senas,Descripcion, Compania, Activo)
+     VALUES ('Phischel Central','Cartago','Centro','Contiguo a la Municipalidad',
+             'Sede central','Farmacias Phischel','1');
+             
+INSERT INTO SUCURSAL (Nombre, Provincia, Ciudad, Senas,Descripcion, Compania, Activo)
+     VALUES ('Phischel PZ','San José','Pérez Zeledón','Costado norte de la escuela doce de Marzo',
+             'Sede PZ','Farmacias Phischel','1');
+
+INSERT INTO SUCURSAL (Nombre, Provincia, Ciudad, Senas,Descripcion, Compania, Activo)
+     VALUES ('BombaTica Alajuela','Alajuela','Poas','Costado sur del estadio Alejandro Morera Soto'
+           ,'BombaTica LDA','BombaTica','1');
+
+INSERT INTO SUCURSAL (Nombre, Provincia, Ciudad, Senas,Descripcion, Compania, Activo)
+     VALUES ('BombaTica Nicoya','Guanacaste','Nicoya','Contiguo al estadio Edgardo Baltodano'
+           ,'Sede Nicoya','BombaTica','1');
+
+INSERT INTO ROL(Nombre,Descripcion,Activo)
+     VALUES('Administrador','Administrador acceso total','1');
+
+INSERT INTO ROL(Nombre,Descripcion,Activo)
+     VALUES('Farmaceutico','Farmaceutico con acceso en sucursales','1');
+
+INSERT INTO ROL(Nombre,Descripcion,Activo)
+     VALUES('Doctor','Atencion a los clientes','1');
+
+INSERT INTO ROL(Nombre,Descripcion,Activo)
+     VALUES('Cajero','Control de dinero','1');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (303150415,'Pedro','Alberto','Perez','Lopez','Cartago','Tejar'
+           ,'100 metros sur de Restaurante El Quijongo','15-02-1980'
+		   ,md5('1234'),'Phischel Central','1','Administrador');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (108490249,'Jose','Julian','Arias','Mora','San Jose','San Pedro'
+           ,'Contiguo UCR','01-10-1970'
+		   ,md5('12345'),'BombaTica Alajuela','1','Administrador');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (205911452,'Marco','Luis','Quesada','Rojas','Heredia','Belen'
+           ,'La Rivera de Belen','10-01-1990'
+		   ,md5('123456'),'Phischel Central','1','Farmaceutico');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (405670923,'Ronny','Fabian','Romero','Nuñez','Limon','Sixaola'
+           ,'contiguo a la iglesia sixaola','23-06-1987'
+		   ,md5('1234567'),'Phischel PZ','1','Farmaceutico');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (505821723,'Olman','Dennis','Fajardo','Calvo','Alajuela','Poas'
+           ,'carretera al volcan Poas','19-08-1973'
+		   ,md5('a12d'),'BombaTica Nicoya','1','Farmaceutico');
+
+INSERT INTO EMPLEADO(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia,
+	Cuidad,Senas,FechaNacimiento,Contrasena,Sucursal,Activo,Rol)
+     VALUES (614580522,'Allen','Saul','McDonald','Brenes','Puntarenas','Sardinal'
+           ,'ruta a Caldera','04-07-1992'
+		   ,md5 ('9gfa'),'BombaTica Alajuela','1','Farmaceutico');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('Bayer','USA','1');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('Novaris','CR','1');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('Roche','USA','1');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('Pfizer','Mexico','1');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('Merck','USA','1');
+
+INSERT INTO PROVEEDOR(Nombre,Sede,Activo)
+     VALUES('SANOFI','CR','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Panadol ExtraFuerte',200,'0','Bayer','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Gex Noche',175,'0','SANOFI','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Gex Día',225,'0','Pfizer','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Inyección Insulina',12000,'1','SANOFI','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Alka Seltzer',75,'0','Roche','1');
+
+INSERT INTO MEDICAMENTO(Nombre,Precio,Prescripcion,Proveedor,Activo)
+     VALUES ('Panadol ULTRA',350,'0','Novaris','1');
+
+INSERT INTO CLIENTE(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia
+           ,Cuidad,Senas,FechaNacimiento,Prioridad,Contrasena,Activo)
+     VALUES (305430785,'Raul','De los Angeles','Arias','Quesada','Cartago','Bermejo'
+           ,'150 metros norte de la escuela de Bermejo','02-06-1996',1
+		   ,md5 ('rolo'),'1');
+
+INSERT INTO CLIENTE(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia
+           ,Cuidad,Senas,FechaNacimiento,Prioridad,Contrasena,Activo)
+     VALUES (509450161,'Rony','Jose','Paniagua','Colindres','Guanacaste','Nandayure'
+           ,'500 metros sur de la pulperia Don Paco','03-09-1996',1
+		   ,md5 ('pani'),'1');
+
+INSERT INTO CLIENTE(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia
+           ,Cuidad,Senas,FechaNacimiento,Prioridad,Contrasena,Activo)
+     VALUES (116220539,'Bryan','Stephen','Abarca','Huever','San Jose','Perez Zeledon'
+           ,'contiguo al bar Mis Cositas','07-11-1997',1
+		   ,md5 ('huever'),'1');
+
+INSERT INTO CLIENTE(Cedula,Nombre1,Nombre2,Apellido1,Apellido2,Provincia
+           ,Cuidad,Senas,FechaNacimiento,Prioridad,Contrasena,Activo)
+     VALUES (306380922,'Ronny','Nicky','Quesada','Arias','Cartago','Coris'
+           ,'350 metros este de la escuela de Coris','23-09-1996',1
+		   ,md5 ('nicky'),'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(509450161,80564738,'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(509450161,25730904,'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(306380922,70837343,'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(306380922,22478490,'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(305430785,70537424,'1');
+
+INSERT INTO TELEFONOXCLIENTE(Cliente,Telefono,Activo)
+     VALUES(116220539,60363619,'1');
+
+INSERT INTO PADECIMIENTO(Cliente,Padecimiento,Ano,Activo)
+     VALUES(509450161,'Bronquitis',2000,'1');
+
+INSERT INTO PADECIMIENTO(Cliente,Padecimiento,Ano,Activo)
+     VALUES(306380922,'Diabetes',2005,'1');
+
+INSERT INTO PADECIMIENTO(Cliente,Padecimiento,Ano,Activo)
+     VALUES(509450161,'Asma',1998,'1');
+
+INSERT INTO PADECIMIENTO(Cliente,Padecimiento,Ano,Activo)
+     VALUES(116220539,'Conjunivitis',2010,'1');
+     
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(1,'Phischel Central','1');
+  
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(2,'Phischel Central','1');
+   
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(3,'Phischel PZ','1');
+   
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(4,'Phischel PZ','1');
+    
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(5,'BombaTica Alajuela','1');
+  
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(6,'BombaTica Alajuela','1');
+   
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(7,'BombaTica Nicoya','1');
+   
+INSERT INTO CAJA(Id,Sucursal,Activo)
+	VALUES(8,'BombaTica Nicoya','1');
+    
+INSERT INTO ADMINISTRADORXSUCURSAL(Administrador,Sucursal,Activo)
+     VALUES(108490249,'BombaTica Nicoya','1');
+
+INSERT INTO ADMINISTRADORXSUCURSAL(Administrador,Sucursal,Activo)
+     VALUES(108490249,'BombaTica Alajuela','1');
+
+INSERT INTO ADMINISTRADORXSUCURSAL(Administrador,Sucursal,Activo)
+     VALUES(303150415,'Phischel Central','1');
+
+INSERT INTO ADMINISTRADORXSUCURSAL(Administrador,Sucursal,Activo)
+     VALUES(303150415,'Phischel PZ','1');
+     
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ExtraFuerte','Phischel Central',40,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ExtraFuerte','Phischel PZ',18,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ExtraFuerte','BombaTica Alajuela',80,20,80,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ExtraFuerte','BombaTica Nicoya',50,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Noche','Phischel Central',50,30,60,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Noche','Phischel PZ',29,40,70,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Noche','BombaTica Alajuela',40,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Noche','BombaTica Nicoya',36,15,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Día','Phischel Central',50,40,80,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Día','Phischel PZ',35,15,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Día','BombaTica Alajuela',40,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Gex Día','BombaTica Nicoya',27,20,40,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Inyección Insulina','Phischel Central',10,15,20,'1');
+   
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Inyección Insulina','Phischel PZ',7,10,15,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Inyección Insulina','BombaTica Alajuela',19,20,35,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Inyección Insulina','BombaTica Nicoya',6,10,20,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Alka Seltzer','Phischel Central',50,30,60,'1');
+   
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Alka Seltzer','Phischel PZ',37,50,50,'1');
+   
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Alka Seltzer','BombaTica Alajuela',35,30,65,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Alka Seltzer','BombaTica Nicoya',35,15,40,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ULTRA','Phischel Central',32,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ULTRA','Phischel PZ',15,20,50,'1');
+  
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ULTRA','BombaTica Alajuela',28,20,50,'1');
+    
+INSERT INTO MEDICAMENTOXSUCURSAL(Medicamento,Sucursal,Cantidad,StockMinimo,StockPromedio,Activo)
+	VALUES('Panadol ULTRA','BombaTica Nicoya',5,20,50,'1');
